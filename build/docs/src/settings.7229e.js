10ad9a14bbbfbad873a9abb612bfd01aac1c813f
Nikolaenko Sergey <nikolaenkoserzh@gmail.com>
2025-10-06 23:40:46 +0800

    Add GitHub Pages build

---
+++ build/docs/src/settings.7229e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/mainScene.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"ebee1",main:"714d8"}};
